3f045ff6306834585ca3a89cbbcd0bb7e5d465c5
Mathis <eyzex@hotmail.fr>
2023-01-02 11:14:15 +0100

    feat(app): added custom slash screen

--- App.jsx
+++ App.jsx
@@ -6,11 +6,42 @@ import { supabase } from './supabase';
 import AccountScreen from './pages/AccountScreen';
 import AppointmentScreen from './pages/AppointmentScreen';
 import { setNotificationHandler } from 'expo-notifications';
+import * as SplashScreen from 'expo-splash-screen';
+import { Text, View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
-
+SplashScreen.preventAutoHideAsync();
 export default function App() {
   const [session, setSession] = React.useState(null);
+  const [isAppReady, setIsAppReady] = React.useState(false);
+
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+          setSession(session);
+        });
+        supabase.auth.onAuthStateChange((_event, session) => {
+          setSession(session);
+        });
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsAppReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (isAppReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isAppReady]);
+  if (!isAppReady) {
+    return null;
+  }
 
   setNotificationHandler({
     handleNotification: async () => ({
@@ -20,17 +51,8 @@ export default function App() {
     }),
   });
 
-  React.useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={onLayoutRootView}>
       <Stack.Navigator>
         {session && session.user ? (
           <Stack.Screen
